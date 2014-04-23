6368830fb252acaaa021052aa0a5a100a67bf650
David Barajas <davidbt@gmail.com>
2014-04-23 14:51:14 -0500

    Search between adjacent records

--- hg19/models.py
+++ hg19/models.py
@@ -1,7 +1,43 @@
 from django.db import models
+from django.db import connection
+
+class SequenceManager(models.Manager):
+    def get_db_where_ors(self, s):
+        ors = []
+        for i in range(1,len(s)):
+            pre = '%' + s[:i].replace("'", "''")
+            pos = s[i:].replace("'", "''") + '%'
+            ors.append("(s1.seq like '{pre}' and s2.seq like '{pos}')".format(pre=pre, pos=pos))
+        return ors
+
+    def search_between(self, S, seq):
+        """Search between adjacent records.
+        param S is the Sequence class
+        """
+
+        seq = seq.replace("'", "''")
+        query = """
+        select s1.id, s1.seq || s2.seq as combined_seq 
+        from {table_name} s1
+            inner join {table_name} s2 on (s1.id + 1) = s2.id
+        where s1.seq like {seq} 
+        """.replace('{table_name}', S._meta.db_table).replace('{seq}', "'%"+seq+"%'")
+
+        ors = self.get_db_where_ors(seq)
+        str_ors = '\nor '.join(ors)
+        query += "\nor " + str_ors
+
+        cursor = connection.cursor()
+        cursor.execute(query)
+        rows = []
+        for row in cursor.fetchall():
+            rows.append(row)
+        return rows
 
 class Sequence(models.Model):
     seq = models.TextField()
+
+    objects = SequenceManager()
 
     def __unicode__(self):
         return u"{seq}".format(seq=self.seq)
