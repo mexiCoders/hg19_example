05bc0e0121dfc52b2e3f315769b652f0a6c44a5a
David Barajas <davidbt@gmail.com>
2014-06-03 11:21:07 -0500

    Added local alignment page

--- hg19/forms.py
+++ hg19/forms.py
@@ -7,3 +7,14 @@ class SearchForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(SearchForm, self).__init__(*args, **kwargs)
         self.fields['chromosome'].choices = [('', 'Any')] +  [(i, i) for i in range(1, 23)] + [('X', 'X'), ('Y', 'Y'), ('M', 'M')]
+
+
+class LocalAlignmentForm(forms.Form):
+    seq = forms.CharField(required=False, label='DNA sequence', min_length=12, max_length=200)
+    chromosome = forms.ChoiceField(label='Chromosome', required=False)
+    search_method = forms.ChoiceField(label='Search method', required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(LocalAlignmentForm, self).__init__(*args, **kwargs)
+        self.fields['chromosome'].choices = [('', 'Any')] +  [(i, i) for i in range(1, 23)] + [('X', 'X'), ('Y', 'Y'), ('M', 'M')]
+        self.fields['search_method'].choices = [('PostBIS', 'PostBIS'), ('Postgresql', 'Postgresql')]
